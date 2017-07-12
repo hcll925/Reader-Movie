cbb500d69b1bdbfb794a21e5d9578950a985cbdf
hcll925 <hc_my5ill@163.com>
2017-07-12 10:47:19 +0800

    文章详情页开发
    Signed-off-by: hcll925 <hc_my5ill@163.com>

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -16,7 +16,7 @@ Page({
   onPostTap:function(event) {
       var postId = event.currentTarget.dataset.postid;
       wx.navigateTo({
-          url: "post-detail/post-detail"
+          url: "post-detail/post-detail?id=" + postId
       })
   }
 })
