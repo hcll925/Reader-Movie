ba422083eff8e358f32a4d2ceb1cb81f888c7974
hcll925 <hc_my5ill@163.com>
2017-07-13 17:32:44 +0800

    通过豆瓣API获取正在热映电影
    Signed-off-by: hcll925 <hc_my5ill@163.com>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ App({
     globalData: {
         g_isPlayingMusic: false,
         g_currentMusicPostId: null,
+        doubanBase: "https://api.douban.com",
     }
 })
 
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,48 @@
+var app = getApp();
 Page({
+    onLoad: function (event) {
+        var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+        var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+        var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
+        this.getMovieListData(inTheatersUrl);
+        // this.getMovieListData(comingSoonUrl);
+        // this.getMovieListData(top250Url);
+    },
+
+    getMovieListData: function (url) {
+        var that = this;
+        wx.request({
+            url: url,
+            method: 'GET',
+            header: {
+                'Content-Type': 'json'
+            },
+            success: function (res) {
+                console.log(res);
+                that.processDoubanData(res.data);
+            }
+        })
+    },
+
+    processDoubanData: function (moviesDouban) {
+        var movies = [];
+        for (var idx in moviesDouban.subjects) {
+            var subject = moviesDouban.subjects[idx];
+            var title = subject.title;
+            if (title.length >= 6) {
+                title = title.substring(0, 6) + '...';
+            }
+            var temp = {
+                title: title,
+                average: subject.rating.average,
+                coverageUrl: subject.images.large,
+                movieId: subject.id,
+            }
+            movies.push(temp)
+        }
+        this.setData({
+            movies: movies
+        });
+    }
 })
